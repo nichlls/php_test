cef1526e92d2c3f50697c316ba47c52206108902
nichlls <107227159+nichlls@users.noreply.github.com>
2026-01-16 15:17:56 +0000

    Implement NotificationInterface

--- src/Notification.php
+++ src/Notification.php
@@ -4,8 +4,23 @@ readonly class Notification implements NotificationInterface
 {
     public function __construct(
         private NotificationType $type,
-        private string           $recipient,
-        private string           $message
-    ){
+        private string $recipient,
+        private string $message
+    ) {
+    }
+
+    public function getType(): NotificationType
+    {
+        return $this->type;
+    }
+
+    public function getRecipient(): string
+    {
+        return $this->recipient;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
     }
 }
